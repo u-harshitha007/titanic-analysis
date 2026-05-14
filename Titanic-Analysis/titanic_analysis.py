5a140130b77e1b347f7fa6c091f7a410063a1332
u-harshitha007 <harshithauppar203@gmail.com>
2026-05-14 19:10:11 +0530

    Added survival visualization by gender

--- Titanic-Analysis/titanic_analysis.py
+++ Titanic-Analysis/titanic_analysis.py
@@ -9,3 +9,16 @@ df["Age"].fillna(df["Age"].mean(), inplace=True)
 print(df.groupby("Sex")["Survived"].mean())
 
 df.to_csv("cleaned_titanic.csv", index=False)
+
+import matplotlib.pyplot as plt
+
+# Survival by Gender
+survival_by_gender = df.groupby("Sex")["Survived"].mean()
+
+survival_by_gender.plot(kind="bar")
+
+plt.title("Survival Rate by Gender")
+plt.xlabel("Gender")
+plt.ylabel("Survival Rate")
+
+plt.show()
